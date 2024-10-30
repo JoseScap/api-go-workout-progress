4772c55b358b12abe2ad6e5c16e714c118079216
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-10-29 23:33:57 -0300

    implement root router

--- main.go
+++ main.go
@@ -1,17 +1,11 @@
 package main
 
 import (
+	"go-workout-progress/infrastructure/routers"
 	"net/http"
-
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
 )
 
 func main() {
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello Workout tracker"))
-	})
-	http.ListenAndServe(":3000", r)
+	router := routers.RootRouter()
+	http.ListenAndServe(":3000", router)
 }
