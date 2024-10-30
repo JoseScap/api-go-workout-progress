95d195cfb5919be3843954dc75ac26c88113d191
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-10-30 12:57:41 -0300

    add exercise model

---
+++ domain/models/exercise.go
@@ -0,0 +1,39 @@
+package models
+
+import (
+	"time"
+
+	"github.com/gobuffalo/pop/v6"
+	"github.com/gobuffalo/validate/v3"
+	"github.com/gofrs/uuid"
+)
+
+// Exercise is used by pop to map your exercises database table to your go code.
+type Exercise struct {
+	ID        uuid.UUID `json:"id" db:"id"`
+	CreatedAt time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" db:"updated_at"`
+	Name      string    `json:"name" db:"name"`
+	Metric    string    `json:"metric" db:"metric"`
+}
+
+// Exercises is not required by pop and may be deleted
+type Exercises []Exercise
+
+// Validate gets run every time you call a "pop.Validate*" (pop.ValidateAndSave, pop.ValidateAndCreate, pop.ValidateAndUpdate) method.
+// This method is not required and may be deleted.
+func (e *Exercise) Validate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateCreate gets run every time you call "pop.ValidateAndCreate" method.
+// This method is not required and may be deleted.
+func (e *Exercise) ValidateCreate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
+
+// ValidateUpdate gets run every time you call "pop.ValidateAndUpdate" method.
+// This method is not required and may be deleted.
+func (e *Exercise) ValidateUpdate(tx *pop.Connection) (*validate.Errors, error) {
+	return validate.NewErrors(), nil
+}
