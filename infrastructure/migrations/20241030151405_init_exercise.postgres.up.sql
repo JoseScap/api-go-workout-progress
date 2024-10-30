94f375fec527e301cf853a52775cb86b16a7eb27
Juan Jose Puente Scapolatempo <juanpuentescapolatempo@gmail.com>
2024-10-30 13:11:55 -0300

    update cript

--- infrastructure/migrations/20241030151405_init_exercise.postgres.up.sql
+++ infrastructure/migrations/20241030151405_init_exercise.postgres.up.sql
@@ -1,7 +1,8 @@
 create table exercises (
-    id          uuid not null primary key,
-    created_at  timestamp without time zone not null,
-    updated_at  timestamp without time zone not null,
-    "name"        varchar(128) not null unique,
-    metric      varchar(128)
+  id          uuid not null primary key default gen_random_uuid(),
+  created_at  timestamp without time zone not null default now(),
+  updated_at  timestamp without time zone not null default now(),
+  "name"      varchar(128) not null unique,
+  metric      varchar(128) not null,
+  category    varchar(16) not null unique 
 );
